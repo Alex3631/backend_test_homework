6bbaa6170535bcf64dd8d46f91009231dacbfa0e
Стас Басов <stas.basov@whatever.com>
2022-11-11 19:02:14 +0300

    My two commit: change main.py

--- program.py
+++ program.py
@@ -1,9 +1,1 @@
 print('Я домашка')
-# Делаем первый коммит, в кавычках пишем комментарий.
-git commit -m "First commit: change program.py"
-
-# Добавили файлы в индекс Git.
-git add .
-
-# Добавили эти файлы к предыдущему коммиту.
-git commit --amend -m "First commit: new files added" 
